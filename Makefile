f8fa4ca0b2e8ee1efced574913a9e892e62cae3f
Jose V Beneyto <sepen@crux.nu>
2008-10-03 11:01:17 +0000

    Added ck4dns tool.
    git-svn-id: file:///tmp/dyndnsup-svn@10 fd3b5e10-894f-11dd-9b1c-353368435786

--- Makefile
+++ Makefile
@@ -3,12 +3,14 @@
 
 DESTDIR=
 BINDIR=/usr/bin
-ETCDIR=/etc
+ETCDIR=/etc/dyndnsup
 MANDIR=/usr/share/man
 LOGDIR=/var/log
 SHEBANG=/bin/sh
 
 .PHONY: all install clean uninstall
+
+all: dyndnsup ck4dns
 
 dyndnsup: src/dyndnsup.sh
 	@sed -e "s|#SHEBANG#|$(SHEBANG)|" \
@@ -16,23 +18,29 @@ dyndnsup: src/dyndnsup.sh
 			 -e "s|#LOGDIR#|$(LOGDIR)|g" \
 			 src/dyndnsup.sh > dyndnsup
 
-all: dyndnsup
+ck4dns: src/ck4dns.sh
+	@sed -e "s|#SHEBANG#|$(SHEBANG)|" \
+			 -e "s|#ETCDIR#|$(ETCDIR)|g" \
+			 -e "s|#DYNDNSUP#|$(DESTDIR)$(BINDIR)/dyndnsup|g" \
+			 src/ck4dns.sh > ck4dns
 
 install: all
 	@install -d $(DESTDIR)$(BINDIR)
 	@install -m 0755 dyndnsup $(DESTDIR)$(BINDIR)/dyndnsup
+	@install -m 0755 ck4dns $(DESTDIR)$(BINDIR)/ck4dns
 	@install -d $(DESTDIR)$(ETCDIR)
 	@install -m 0600 src/dyndnsup.conf $(DESTDIR)$(ETCDIR)/dyndnsup.conf
+	@install -m 0600 src/ck4dns.conf $(DESTDIR)$(ETCDIR)/ck4dns.conf
 	@install -d $(DESTDIR)$(MANDIR)/man1
 	@install -m 0644 src/dyndnsup.1 $(DESTDIR)$(MANDIR)/man1/dyndnsup.1
 
 clean:
-	@rm -f dyndnsup
+	@rm -f dyndnsup ck4dns
 	@rm -f */*~ *~
 
 uninstall:
-	@rm -f $(DESTDIR)$(BINDIR)/dyndnsup
-	@rm -f $(DESTDIR)$(ETCDIR)/dyndnsup.conf
+	@rm -f $(DESTDIR)$(BINDIR)/{dyndnsup,ck4dns}
+	@rm -f $(DESTDIR)$(ETCDIR)/dyndnsup.conf,ck4dns.conf}
 	@rm -f $(DESTDIR)$(MANDIR)/man1/dyndnsup.1
 
 # End of file
---
+++ src/ck4dns.sh
@@ -0,0 +1,64 @@
+#!#SHEBANG#
+#
+# Copyright (c) 2007 by Jose V. Beneyto, sepen at users dot sourceforge dot net
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+msgError() {
+  echo "Error, $@" 2>&1
+  exit 1
+}
+
+msgUsage() {
+  echo "Usage: $APPNAME [-h] | [-c file]"
+  echo "Read the manual for futher information: $APPNAME(1)"
+  exit 0
+}
+
+main() {
+  local dhost dconf
+  if [ $# -gt 0 ]; then
+    case $1 in
+      -c) CONFIG_FILE="$2" ;;
+       *) msgUsage ;;
+    esac
+  fi
+  [ ! -r "$CONFIG_FILE" ] && msgError "can't read config file '$CONFIG_FILE'"
+  while read line; do
+    if [ "$(echo $line | grep -ve '^#')" ]; then
+    dhost="$(echo $line | awk '{print $1}')"
+    dconf="$(echo $line | awk '{print $2}')"
+    [ -z "$dhost" -a -z "$dconf" ] && msgError "while parsing '$CONFIG_FILE'"
+    [ ! -r "$CONFIG_FILE" ] && msgError "can't read config file '$dconf'"
+    host $i 2>&1 >/dev/null
+    if [ $? -ne 0 ]; then
+        $DYNDNSUP_BIN -v -c $dconf
+  fi
+  done < $CONFIG_FILE
+}
+
+APPNAME="$(basename $0)"
+APPVERSION="0.2.2"
+
+CONFIG_FILE="#ETCDIR#/$APPNAME.conf"
+DYNDNSUP_BIN="#DYNDNSUP#"
+
+main $@ 2>&1
+
+# End of file
